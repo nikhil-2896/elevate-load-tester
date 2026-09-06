7f2f3ec8e1959416def329e70f7599101a182458
Nikhil Khosla <nikhilkh06@gmail.com>
2026-09-06 22:36:48 +0530

    Point frontend to Cloudflare tunnel HTTPS URL

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -7,7 +7,7 @@ import WebsiteLoadTestSection from "./components/WebsiteLoadTestSection";
 import LoadTestConfig from "./components/LoadTestConfig";
 import { colors, font, space } from "./theme";
 
-const BASE = "http://13.63.14.208:30231";
+const BASE = "https://pig-way-gamecube-shared.trycloudflare.com";
 
 export default function App() {
   const [url, setUrl] = useState("https://httpbin.org/post");
